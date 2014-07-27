4931a1f7a6c9186d03f0c60ac28a43fc1b9d1350
KRYS MATHIS <krysmathis@gmail.com>
2014-07-27 08:56:41 -0500

    Set up development vs production env for dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,18 +1,23 @@
 class Listing < ActiveRecord::Base
-has_attached_file 	:image, :styles => { :medium => "200x200", :thumb => "100x>" }, :default_url => "default.jpg",
-					:storage => :dropbox,
-				    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-#				    :dropbox_options => {...}
+if Rails.env.development?
+	has_attached_file 	:image, :styles => { :medium => "200x200", :thumb => "100x>" }, :default_url => "default.jpg"
+	validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+
+else
+	has_attached_file 	:image, :styles => { :medium => "200x200", :thumb => "100x>" }, :default_url => "default.jpg",
+						:storage => :dropbox,
+					    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+	#				    :dropbox_options => {...}
+						:path => ":style/:id_:filename"
 
 
+	#validates_attachment_content_type :image, :content_type => "/^image/"
 
-#validates_attachment_content_type :image, :content_type => "/^image/"
 
+	#:dropbox_options => {...}
 
-#:dropbox_options => {...}
-
-#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
-
+	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates_attachment_content_type :image, :content_type => ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+end
 
 end
